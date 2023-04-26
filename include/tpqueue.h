ad9f7d0d8707e2f898be06d8a8071fed68c4d131
RudixMasha <116712267+RudixMasha@users.noreply.github.com>
2023-04-26 18:59:43 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,9 +4,55 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+ private:
+    struct QItem {
+        T x;
+        QItem* next;
+    };
+    QItem* head;
 
+public:
+  TPQueue() : head(nullptr) {}
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+  void push(const T& value) {
+    if (isEmpty()) {
+      QItem* temp = new QItem;
+      temp->x = value;
+      temp->next = nullptr;
+            head = temp;
+        } else if (head->x.prior < value.prior) {
+         QItem* temp = new QItem;
+         temp->x = value;
+         temp->next = head;
+         head = temp;
+        } else {
+         QItem* cur = head;
+         while (cur->next != nullptr && value.prior = cur->next->x.prior)
+           cur = cur->next;
+         QItem* temp = new QItem;
+         temp->x = value;
+         temp->next = cur->next;
+         cur->next = temp;
+        }
+    }
+    T pop() {
+        if (isEmpty()) {
+            throw std::string("Empty!")
+        } else {
+            QItem* temp = head->next;
+            T value = head->x;
+            delete(head);
+            head = temp;
+            return value;
+        }
+    }
+    ~TPQueue() {
+        while (head != nullptr)
+            pop();
+    }
+};
 struct SYM {
   char ch;
   int prior;
